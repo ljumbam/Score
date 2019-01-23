1c4010461c55fe3f0b7ab7811a8dc3d4e4b26b50
Loayeh Jumbam <ljumbam@esri.com>
2019-01-22 22:17:19 -0800

    Initial commit of project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+from distutils.core import setup
+
+DESCRIPTION = 'A tool for writing music.'
+
+setup(
+    name='score',
+    version='0.0.1',
+    author='Loayeh Jumbam',
+    author_email='loayeh@algotunes.com',
+    packages=setuptools.find_packages(),
+    license=open('LICENSE.txt'),
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+)
